d3b38024cf75d6ed76f18c65d3872a0b9ca35b2f
Azmath Ali <mohdazmathali20@gmail.com>
2024-07-04 12:59:21 +0530

    changes done

---
+++ Frontend/src/utils/constant.jsx
@@ -0,0 +1,18 @@
+
+
+export const API_END_POINT = "https://netflix-2-0-xjlk.onrender.com"
+
+
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjYxYjlhYjA4ZGQ2M2UzOTA5N2Y5Nzk3NjQ5MTUyZCIsIm5iZiI6MTcxOTI5ODUzMS4wMjYxNzEsInN1YiI6IjY2MzExZTNkNGMyNTkyMDEyYWM4Zjc1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YYt-E9DVpWjE1cYm1jho899QtCl7yd7n4AQuqGI_LU8'
+    }
+  };
+
+
+  export const Now_Playing = "https://api.themoviedb.org/3/movie/now_playing"
+  export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular"
+  export const Top_Rated = "https://api.themoviedb.org/3/movie/top_rated"
+  export const Up_Coming = "https://api.themoviedb.org/3/movie/upcoming"
